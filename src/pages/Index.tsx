7d2a7063af73f5a7349c5aa708bed690abaf58b4
scoobydoopydoo5 <scoobydoopydoo5@gmail.com>
2025-10-16 22:04:13 +0300

    asddas

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -25,7 +25,7 @@ const Index = () => {
           <CheckSquare className="w-12 h-12 text-primary" />
         </div>
         <h1 className="text-5xl sm:text-6xl font-bold text-foreground mb-6 bg-clip-text">
-          U4 Bio Checklists+
+          Ace U4 Bio
         </h1>
         <p className="text-xl sm:text-2xl text-muted-foreground mb-10 leading-relaxed">
           Master Edexcel Unit 4 IAL Biology with interactive checklists, past
